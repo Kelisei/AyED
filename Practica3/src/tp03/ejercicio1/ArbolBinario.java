4ccaa1d6bc81d627a0f6688befbc98a03f10ef67
FRANCISCO GONZALO PERCARA <frankestudios71@gmail.com>
2023-04-20 10:27:36 -0300

    Fantasma

---
+++ Practica3/src/tp03/ejercicio1/ArbolBinario.java
@@ -0,0 +1,99 @@
+package tp03.ejercicio1;
+
+public class ArbolBinario<T> {
+	private T dato;
+	private ArbolBinario<T> hijoIzquierdo;   
+	private ArbolBinario<T> hijoDerecho; 
+
+	
+	public ArbolBinario() {
+		super();
+	}
+
+	public ArbolBinario(T dato) {
+		this.dato = dato;
+	}
+
+	/*
+	 * getters y setters
+	 * 
+	 */
+	public T getDato() {
+		return dato;
+	}
+
+	public void setDato(T dato) {
+		this.dato = dato;
+	}
+	
+	/**
+	 * Preguntar antes de invocar si tieneHijoIzquierdo()
+	 * @return
+	 */
+	public ArbolBinario<T> getHijoIzquierdo() {
+		return this.hijoIzquierdo;
+	}
+
+	public ArbolBinario<T> getHijoDerecho() {
+		return this.hijoDerecho;
+
+	}
+
+	public void agregarHijoIzquierdo(ArbolBinario<T> hijo) {
+		this.hijoIzquierdo = hijo;
+	}
+
+	public void agregarHijoDerecho(ArbolBinario<T> hijo) {
+		this.hijoDerecho = hijo;
+	}
+
+	public void eliminarHijoIzquierdo() {
+		this.hijoIzquierdo = null;
+	}
+
+	public void eliminarHijoDerecho() {
+		this.hijoDerecho = null;
+	}
+
+	public boolean esVacio() {
+		return this.getDato() == null && !this.tieneHijoIzquierdo() && !this.tieneHijoDerecho();
+	}
+
+	public boolean esHoja() {
+		return (!this.tieneHijoIzquierdo() && !this.tieneHijoDerecho());
+
+	}
+
+	@Override
+	public String toString() {
+		return this.getDato().toString();
+	}
+
+	 
+	public boolean tieneHijoIzquierdo() {
+		return this.hijoIzquierdo!=null;
+	}
+
+	 
+	public boolean tieneHijoDerecho() {
+		return this.hijoDerecho!=null;
+	}
+
+	public int contarHojas() {
+		return 0;
+	}
+	
+
+    public ArbolBinario<T> espejo() {
+		
+		return null;
+	}
+
+
+	public void entreNiveles(int n, int m){
+		
+	}
+
+	
+
+}
